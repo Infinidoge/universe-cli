8d7013024cbf7e84b0d4b23d9a20228850f1f1a3
Infinidoge <infinidoge@inx.moe>
2023-07-27 01:00:31 -0400

    reduce indentation in flake

--- flake.nix
+++ flake.nix
@@ -16,51 +16,50 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = inputs@{ self, nixpkgs, crane, flake-parts, rust-overlay, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-      ];
+  outputs = inputs@{ self, nixpkgs, crane, flake-parts, rust-overlay, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [
+      "x86_64-linux"
+    ];
 
-      imports = [
-        inputs.devshell.flakeModule
-      ];
+    imports = [
+      inputs.devshell.flakeModule
+    ];
 
-      perSystem = { config, pkgs, system, ... }:
-        let
-          rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-            extensions = [ "rust-src" ];
-          };
-
-          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-
-          commonArgs = {
-            src = craneLib.cleanCargoSource (craneLib.path ./.);
-          };
-          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-          universe-cli = craneLib.buildPackage (commonArgs // {
-            inherit cargoArtifacts;
-            meta.mainProgram = "universe-cli";
-          });
-        in
-        {
-          _module.args.pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ rust-overlay.overlays.default ];
-          };
-
-          packages.default = universe-cli;
-          apps.default.program = config.packages.default;
-
-          devshells.default.devshell = {
-            # name = "${universe-cli.meta.mainProgram}-devshell";
-
-            packagesFrom = [ universe-cli ];
-
-            packages = with pkgs; [
-              rustToolchain
-            ];
-          };
+    perSystem = { config, pkgs, system, ... }:
+      let
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" ];
         };
-    };
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        commonArgs = {
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        universe-cli = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+          meta.mainProgram = "universe-cli";
+        });
+      in
+      {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+
+        packages.default = universe-cli;
+        apps.default.program = config.packages.default;
+
+        devshells.default.devshell = {
+          # name = "${universe-cli.meta.mainProgram}-devshell";
+
+          packagesFrom = [ universe-cli ];
+
+          packages = with pkgs; [
+            rustToolchain
+          ];
+        };
+      };
+  };
 }
