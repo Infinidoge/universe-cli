b9630a12bef59791928f8d0e802762d371d33d7e
Infinidoge <infinidoge@inx.moe>
2024-04-22 14:01:53 -0400

    update: use newer cli for nix flake update

--- src/update.rs
+++ src/update.rs
@@ -3,33 +3,18 @@ use crate::Cli;
 
 pub(crate) fn command_update(cli: &Cli, inputs: &Option<Vec<String>>) -> CliResult<()> {
     let mut command = build_nix_command(cli)?;
-    command.arg("flake");
+    command.arg("flake").arg("update");
 
-    match inputs {
-        Some(inputs) => {
-            println!("Updating inputs: {}", inputs.join(", "));
-
-            let input_args = inputs
-                .into_iter()
-                .flat_map(|input| vec!["--update-input".to_owned(), input.to_owned()])
-                .collect::<Vec<String>>();
-
-            command.arg("lock").args(input_args);
-
-            if !command.spawn()?.wait()?.success() {
-                return Err(UniverseCliError::FailedToExecuteNix);
-            };
-        }
-        None => {
-            println!("Updating all inputs");
-
-            command.arg("update");
-
-            if !command.spawn()?.wait()?.success() {
-                return Err(UniverseCliError::FailedToExecuteNix);
-            };
-        }
+    if let Some(inputs) = inputs {
+        println!("Updating inputs: {}", inputs.join(", "));
+        command.args(inputs);
+    } else {
+        println!("Updating all inputs");
     }
+
+    if !command.spawn()?.wait()?.success() {
+        return Err(UniverseCliError::FailedToExecuteNix);
+    };
 
     Ok(())
 }
