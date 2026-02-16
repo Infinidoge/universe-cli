7a76cf7409a87c08d5bbe03a91c04c4d0c2ef999
Infinidoge <infinidoge@inx.moe>
2026-02-15 19:28:10 -0500

    flake: reformat, change shell name

--- flake.nix
+++ flake.nix
@@ -17,24 +17,33 @@
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [
-      "x86_64-linux"
-    ];
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
 
-    imports = [
-      inputs.devshell.flakeModule
-      ./pkgs.nix
-    ];
+      imports = [
+        inputs.devshell.flakeModule
+        ./pkgs.nix
+      ];
 
-    perSystem = { config, ... }: {
-      apps.default.program = config.packages.default;
+      perSystem =
+        { config, ... }:
+        {
+          apps.default.program = config.packages.default;
 
-      devshells.default.devshell = {
-        name = config.packages.default.meta.mainProgram;
-        motd = "";
-        packagesFrom = [ config.packages.default ];
-      };
+          devshells.default.devshell = {
+            name = "universe-cli-dev";
+            motd = "";
+            packagesFrom = [ config.packages.default ];
+          };
+        };
     };
-  };
 }
